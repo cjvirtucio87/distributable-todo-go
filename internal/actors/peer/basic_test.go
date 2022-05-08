ce2b093a874fb7421a473b8456894a472e5f8e23
CJ Virtucio <cjv287@gmail.com>
2022-05-08 15:02:45 -0400

    refactored to use testCase approach

--- internal/actors/peer/basic_test.go
+++ internal/actors/peer/basic_test.go
@@ -5,123 +5,94 @@ import (
 	"testing"
 )
 
-func testFollowerLogs(t *testing.T, leaderLog rlog.Log, followerLogs []rlog.Log) {
-	for _, followerLog := range followerLogs {
-		expectedEntryCount := 1
-		entries := followerLog.Entries(0, 1)
-		actualEntryCount := len(entries)
-		if actualEntryCount != expectedEntryCount {
-			t.Fatalf("expected [%d], got [%d]", expectedEntryCount, actualEntryCount)
+func TestSendSendsMessageToFollowers(t *testing.T) {
+	testData := []struct{
+		expectedEntryCount int
+		followerLogEntries []rlog.Entry
+		peerCount int
+	}{
+		{
+			1,
+			[]rlog.Entry{
+				rlog.Entry{Command: "doFoo"},
+			},
+			3,
+		},
+		{
+			1,
+			[]rlog.Entry{
+				rlog.Entry{
+					Command: "not supposed to be here",
+				},
+				rlog.Entry{
+					Command: "not supposed to be here either",
+				},
+			},
+			3,
+		},
+	}
+
+	for _, testDatum := range testData {
+		leaderLog := rlog.NewBasicLog()
+		leader := &basicPeer{
+			id:           0,
+			rlog:         leaderLog,
+			NextIndexMap: make(map[int]int),
+			peers:        []Peer{},
 		}
 
-		expectedEntry := leaderLog.Entry(0)
-		for _, actualEntry := range entries {
-			if expectedEntry.Command != actualEntry.Command {
-				t.Fatalf("expected [%s], got [%s]", expectedEntry.Command, actualEntry.Command)
+		followerLogs := make([]rlog.Log, testDatum.peerCount)
+		for i := 0; i < testDatum.peerCount; i++ {
+			if len(testDatum.followerLogEntries) == 0 {
+				followerLogs[i] = rlog.NewBasicLog()
+			} else {
+				followerLogs[i] = rlog.NewBasicLog(
+					rlog.WithBackend(testDatum.followerLogEntries),
+				)
+			}
+		}
+
+		for i := 0; i < testDatum.peerCount; i++ {
+			leader.AddPeer(
+				&basicPeer{
+					id:           i + 1,
+					rlog:         followerLogs[i],
+					NextIndexMap: make(map[int]int),
+					peers:        []Peer{},
+				},
+			)
+		}
+
+		err := leader.Init()
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		err = leader.Send(
+			Message{
+				Entries: []rlog.Entry{
+					rlog.Entry{Command: "doFoo"},
+				},
+			},
+		)
+
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		for _, followerLog := range followerLogs {
+			entries := followerLog.Entries(0, 1)
+			actualEntryCount := len(entries)
+			if actualEntryCount != testDatum.expectedEntryCount {
+				t.Fatalf("expected [%d], got [%d]", testDatum.expectedEntryCount, actualEntryCount)
+			}
+
+			expectedEntry := leaderLog.Entry(0)
+			for _, actualEntry := range entries {
+				if expectedEntry.Command != actualEntry.Command {
+					t.Fatalf("expected [%s], got [%s]", expectedEntry.Command, actualEntry.Command)
+				}
 			}
 		}
 	}
-}
-
-func TestSendSendsMessageToFollowers(t *testing.T) {
-	peerCount := 3
-	leaderLog := rlog.NewBasicLog()
-	leader := &basicPeer{
-		id:           0,
-		rlog:         leaderLog,
-		NextIndexMap: make(map[int]int),
-		peers:        []Peer{},
-	}
-
-	followerLogs := make([]rlog.Log, peerCount)
-	for i := 0; i < peerCount; i++ {
-		followerLogs[i] = rlog.NewBasicLog()
-	}
-
-	for i := 0; i < peerCount; i++ {
-		leader.AddPeer(
-			&basicPeer{
-				id:           i + 1,
-				rlog:         followerLogs[i],
-				NextIndexMap: make(map[int]int),
-				peers:        []Peer{},
-			},
-		)
-	}
-
-	err := leader.Init()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = leader.Send(
-		Message{
-			Entries: []rlog.Entry{
-				rlog.Entry{Command: "doFoo"},
-			},
-		},
-	)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testFollowerLogs(t, leaderLog, followerLogs)
-}
-
-func TestSendDiscardsInvalidFollowerLogEntries(t *testing.T) {
-	peerCount := 3
-	leaderLog := rlog.NewBasicLog()
-	leader := &basicPeer{
-		id:           0,
-		rlog:         leaderLog,
-		NextIndexMap: make(map[int]int),
-		peers:        []Peer{},
-	}
-
-	followerLogs := make([]rlog.Log, peerCount)
-	for i := 0; i < peerCount; i++ {
-		followerLogs[i] = rlog.NewBasicLog(
-			rlog.WithBackend(
-				[]rlog.Entry{
-					rlog.Entry{
-						Command: "not supposed to be here",
-					},
-					rlog.Entry{
-						Command: "not supposed to be here either",
-					},
-				},
-			),
-		)
-	}
-
-	for i := 0; i < peerCount; i++ {
-		leader.AddPeer(
-			&basicPeer{
-				id: i + 1,
-				rlog: followerLogs[i],
-				NextIndexMap: map[int]int{},
-				peers:        []Peer{},
-			},
-		)
-	}
-
-	err := leader.Init()
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	err = leader.Send(
-		Message{
-			Entries: []rlog.Entry{
-				rlog.Entry{Command: "doFoo"},
-			},
-		},
-	)
-
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	testFollowerLogs(t, leaderLog, followerLogs)
 }
